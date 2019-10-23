23139a9c109ad47cd160deba0d56a51198036a63
d1g1t4ld1n4 <dinaelhanan@gmail.com>
2019-10-23 16:34:01 -0400

    Update sponsors.js

--- components/sponsors.js
+++ components/sponsors.js
@@ -31,7 +31,11 @@ const Base = styled(Box)`
 
 const data = {
   sponsors: [
-    
+    {
+      name:'Voiceflow',
+      link: 'https://www.voiceflow.com/',
+      image: 'https://cdn.glitch.com/0ac33e0f-67fc-4d7d-bb15-8c3456d17607%2Fvoiceflow.png?v=1571862832639'
+    }
    
   ],
   additionalSupport: [
