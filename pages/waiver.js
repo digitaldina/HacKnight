4e346c6d590279af9603209b30a4ae2393fffd3d
d1g1t4ld1n4 <dinaelhanan@gmail.com>
2019-11-01 20:30:16 -0400

    Update waiver.js

--- pages/waiver.js
+++ pages/waiver.js
@@ -1,4 +1,2 @@
 import Redirect from 'nextjs-redirect'
-export default class Waiver {
-  Redirect('https://drive.google.com/open?id=1J88tW0jf-VkDY4Su69qi0225u7EjE8wmkHZN9FYlrYM') 
-}
+export default Redirect('https://drive.google.com/open?id=1J88tW0jf-VkDY4Su69qi0225u7EjE8wmkHZN9FYlrYM')
