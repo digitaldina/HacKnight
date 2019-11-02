dcfcf154dbca1f17895bb904487fc42118302bd7
d1g1t4ld1n4 <dinaelhanan@gmail.com>
2019-11-01 20:48:48 -0400

    Update waiver.js

--- pages/waiver.js
+++ pages/waiver.js
@@ -1,2 +1,4 @@
 import Redirect from 'nextjs-redirect'
+if (typeof res.writeHead === 'function') {
 export default Redirect('https://drive.google.com/open?id=1J88tW0jf-VkDY4Su69qi0225u7EjE8wmkHZN9FYlrYM')
+}
