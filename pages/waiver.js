ca38db66787a18487ca9a6207f3a5ff65a477eb6
d1g1t4ld1n4 <dinaelhanan@gmail.com>
2019-11-01 23:43:24 -0400

    Update waiver.js

--- pages/waiver.js
+++ pages/waiver.js
@@ -3,8 +3,8 @@ import Router from 'next/router'
 
 export default class extends React.Component {
   static async getInitialProps({ res }) {
-    if (res) {
-      res.writeHead(302, {
+if (typeof res.writeHead === 'function'){
+  res.writeHead(302, {
         Location: 'https://drive.google.com/open?id=1J88tW0jf-VkDY4Su69qi0225u7EjE8wmkHZN9FYlrYM'
       })
       res.end()
