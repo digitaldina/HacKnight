ff968d13a811686b64cc9bc3eb5f2c539bce4b0f
d1g1t4ld1n4 <dinaelhanan@gmail.com>
2019-11-22 10:53:34 -0500

    Update avatar.js

--- components/avatar.js
+++ components/avatar.js
@@ -3,7 +3,7 @@ import { Image } from 'rebass'
 export default ({ size = 32, light = false, ...props }) => (
   <Image
     {...props}
-    src={`https://cdn.glitch.com/0ac33e0f-67fc-4d7d-bb15-8c3456d17607%2FGroup.svg?v=1572625597700`}
+    src={`https://cdn.glitch.com/0ac33e0f-67fc-4d7d-bb15-8c3456d17607%2Fcrescent-moon.svg?v=1574437998711`}
     alt="HacKnight avatar"
     width={size}
     height={size}
