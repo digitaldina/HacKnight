2faf07d59901d21402852275afb18c9047afb7fd
d1g1t4ld1n4 <dinaelhanan@gmail.com>
2019-11-22 11:30:34 -0500

    Update avatar.js

--- components/avatar.js
+++ components/avatar.js
@@ -4,7 +4,7 @@ export default ({ size = 32, light = false, ...props }) => (
   <Image
     {...props}
     src={`https://cdn.glitch.com/0ac33e0f-67fc-4d7d-bb15-8c3456d17607%2Fcrescent-moon.svg?v=1574437998711`}
-    alt="HacKnight avatar"
+    alt="HackNight avatar"
     width={size}
     height={size}
   />
